664790b3a74f78ff51cd7672e5155007cca3013f
Anton Bui <anhtung.bui@gmail.com>
2025-11-03 00:40:55 +0100

    fix: update API for react-router-dom v6+

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import axios from 'axios';
 import NavigationBar from './components/NavigationBar';
 import Main from './components/Main';
@@ -12,107 +12,97 @@ const FINNHUB_TOKEN = process.env.REACT_APP_FINNHUB_TOKEN;
 // TODO: how to handle error with axios async await
 
 function App() {
-    const [newsArticles, setNewsArticles] = useState([]);
-    const [query, setQuery] = useState('');
-    const [results, setResults] = useState([]);
-    const [loading, setLoading] = useState(false);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const handleFormSubmit = (e, query) => {
-        e.preventDefault();
-        // console.log(query);
-        setQuery(query);
-        // console.log(e.target[0].value);
+  const handleFormSubmit = (e, query) => {
+    e.preventDefault();
+    // console.log(query);
+    setQuery(query);
+    // console.log(e.target[0].value);
+  };
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  useEffect(() => {
+    const getNewsArticles = async () => {
+      setLoading(true);
+      const { data } = await axios.get('https://finnhub.io/api/v1/news?', {
+        params: {
+          category: 'general',
+          token: FINNHUB_TOKEN
+        }
+      });
+      // console.log(data);
+      setNewsArticles(data.slice(0, 10));
+      setLoading(false);
     };
 
-    const handleInputChange = (e) => {
-        setQuery(e.target.value);
+    getNewsArticles();
+  }, []);
+
+  useEffect(() => {
+    const search = async () => {
+      setLoading(true);
+      const { data } = await axios.get('https://finnhub.io/api/v1/search', {
+        params: {
+          q: query,
+          token: FINNHUB_TOKEN
+        }
+      });
+      console.log(data.result);
+      setResults(data.result);
+      setLoading(false);
     };
 
-    useEffect(() => {
-        const getNewsArticles = async () => {
-            setLoading(true);
-            const { data } = await axios.get(
-                'https://finnhub.io/api/v1/news?',
-                {
-                    params: {
-                        category: 'general',
-                        token: FINNHUB_TOKEN,
-                    },
-                }
-            );
-            // console.log(data);
-            setNewsArticles(data.slice(0, 10));
-            setLoading(false);
-        };
+    const timeoutId = setTimeout(() => {
+      if (query) {
+        search();
+      }
+    }, 2000);
 
-        getNewsArticles();
-    }, []);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [query]);
 
-    useEffect(() => {
-        const search = async () => {
-            setLoading(true);
-            const { data } = await axios.get(
-                'https://finnhub.io/api/v1/search',
-                {
-                    params: {
-                        q: query,
-                        token: FINNHUB_TOKEN,
-                    },
-                }
-            );
-            console.log(data.result);
-            setResults(data.result);
-            setLoading(false);
-        };
-
-        const timeoutId = setTimeout(() => {
-            if (query) {
-                search();
+  return (
+    <Router>
+      <header>
+        <NavigationBar
+          query={query}
+          handleFormSubmit={handleFormSubmit}
+          handleInputChange={handleInputChange}
+        />
+      </header>
+      <main className="container">
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={
+              <>
+                <Main query={query} results={results} loading={loading} />
+                <MarketNews newsArticles={newsArticles} loading={loading} />
+              </>
             }
-        }, 2000);
-
-        return () => {
-            clearTimeout(timeoutId);
-        };
-    }, [query]);
-
-    return (
-        <Router>
-            <header>
-                <NavigationBar
-                    query={query}
-                    handleFormSubmit={handleFormSubmit}
-                    handleInputChange={handleInputChange}
-                />
-            </header>
-            <main className="container">
-                <Switch>
-                    <Route path="/" exact>
-                        <Main
-                            query={query}
-                            results={results}
-                            loading={loading}
-                        />
-                        <MarketNews
-                            newsArticles={newsArticles}
-                            loading={loading}
-                        />
-                    </Route>
-                    <Route path="/search">
-                        <Main
-                            query={query}
-                            results={results}
-                            loading={loading}
-                        />
-                    </Route>
-                    <Route path="/:symbol" component={StockDetail} />
-                </Switch>
-            </main>
-            <footer>
-                <Footer />
-            </footer>
-        </Router>
-    );
+          />
+          <Route
+            path="/search"
+            element={<Main query={query} results={results} loading={loading} />}
+          />
+          <Route path="/:symbol" element={<StockDetail />} />
+        </Routes>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/StockDetail.jsx
+++ src/components/StockDetail.jsx
@@ -1,116 +1,109 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import ProfileList from './ProfileList';
 import StockPrice from './StockPrice';
 
 const FINNHUB_TOKEN = process.env.REACT_APP_FINNHUB_TOKEN;
 
-export default function StockDetail({ match }) {
-    let symbol = match.params.symbol;
-    const [profile, setProfile] = useState([]);
-    const [profileLoading, setprofileLoading] = useState(false);
-    const [priceLoading, setPriceLoading] = useState(false);
-    const [chartData, setChartData] = useState(null);
-    const [currentPrice, setCurrentPrice] = useState(null);
-    const [priceDiff, setPriceDiff] = useState(null);
-    const [priceDiffPercentage, setPriceDiffPercentage] = useState(null);
+export default function StockDetail() {
+  const { symbol: rawSymbol } = useParams();
+  const symbol = rawSymbol.replace(/\.[a-z]+/gi, '');
+  const [profile, setProfile] = useState([]);
+  const [profileLoading, setprofileLoading] = useState(false);
+  const [priceLoading, setPriceLoading] = useState(false);
+  const [chartData, setChartData] = useState(null);
+  const [currentPrice, setCurrentPrice] = useState(null);
+  const [priceDiff, setPriceDiff] = useState(null);
+  const [priceDiffPercentage, setPriceDiffPercentage] = useState(null);
 
-    useEffect(() => {
-        getCompanyProfile();
-        getStockPrice();
-    }, []);
+  useEffect(() => {
+    getCompanyProfile();
+    getStockPrice();
+  }, []);
 
-    symbol = symbol.replace(/\.[a-z]+/gi, '');
-
-    const getCompanyProfile = async () => {
-        setprofileLoading(true);
-        const { data } = await axios.get(
-            'https://finnhub.io/api/v1/stock/profile2?',
-            {
-                params: {
-                    symbol: symbol,
-                    token: FINNHUB_TOKEN,
-                },
-            }
-        );
-        // console.log(data);
-        setProfile(data);
-        setprofileLoading(false);
-    };
-
-    const getStockPrice = async () => {
-        setPriceLoading(true);
-        const { data } = await axios.get(
-            'https://finnhub.io/api/v1/stock/candle?',
-            {
-                params: {
-                    symbol: symbol,
-                    resolution: 'D',
-                    from: Date.parse('2020-02-13T00:00:00') / 1000,
-                    to: new Date().getTime(),
-                    token: FINNHUB_TOKEN,
-                },
-            }
-        );
-        // console.log(data);
-
-        const chartData = prepareChartData(data);
-        const {
-            _currentPrice,
-            _priceDiff,
-            _priceDiffPercentage,
-        } = getPriceDiff(data);
-        setCurrentPrice(_currentPrice);
-        setPriceDiff(_priceDiff);
-        setPriceDiffPercentage(_priceDiffPercentage);
-        setChartData(chartData);
-        setPriceLoading(false);
-        // console.log(chartData);
-        // setNews(data);
-    };
-
-    const getPriceDiff = (data) => {
-        const _currentPrice = data.c[data.c.length - 1].toFixed(2);
-        const _previousPrice = data.c[data.c.length - 2].toFixed(2);
-        const _priceDiff = (_currentPrice - _previousPrice).toFixed(2);
-        const _priceDiffPercentage = (
-            (_priceDiff / _previousPrice) *
-            100
-        ).toFixed(2);
-        return {
-            _currentPrice,
-            _previousPrice,
-            _priceDiff,
-            _priceDiffPercentage,
-        };
-    };
-
-    const prepareChartData = (data) => {
-        const chartData = [];
-        for (let i = 0; i < data.t.length; i++) {
-            chartData.push([
-                data.t[i] * 1000,
-                parseFloat(data.c[i].toFixed(2)),
-            ]);
+  const getCompanyProfile = async () => {
+    setprofileLoading(true);
+    const { data } = await axios.get(
+      'https://finnhub.io/api/v1/stock/profile2?',
+      {
+        params: {
+          symbol: symbol,
+          token: FINNHUB_TOKEN
         }
-        return chartData;
-    };
-
-    return (
-        <div className="row mt-3">
-            <div className="col-lg-4 order-2">
-                <ProfileList profile={profile} loading={profileLoading} />
-            </div>
-            <div className="col-lg-8 order-1 mb-3">
-                <StockPrice
-                    profile={profile}
-                    chartData={chartData}
-                    currentPrice={currentPrice}
-                    priceDiff={priceDiff}
-                    priceDiffPercentage={priceDiffPercentage}
-                    loading={priceLoading}
-                />
-            </div>
-        </div>
+      }
     );
+    // console.log(data);
+    setProfile(data);
+    setprofileLoading(false);
+  };
+
+  const getStockPrice = async () => {
+    setPriceLoading(true);
+    const { data } = await axios.get(
+      'https://finnhub.io/api/v1/stock/candle?',
+      {
+        params: {
+          symbol: symbol,
+          resolution: 'D',
+          from: Date.parse('2020-02-13T00:00:00') / 1000,
+          to: new Date().getTime(),
+          token: FINNHUB_TOKEN
+        }
+      }
+    );
+    // console.log(data);
+
+    const chartData = prepareChartData(data);
+    const { _currentPrice, _priceDiff, _priceDiffPercentage } =
+      getPriceDiff(data);
+    setCurrentPrice(_currentPrice);
+    setPriceDiff(_priceDiff);
+    setPriceDiffPercentage(_priceDiffPercentage);
+    setChartData(chartData);
+    setPriceLoading(false);
+    // console.log(chartData);
+    // setNews(data);
+  };
+
+  const getPriceDiff = (data) => {
+    const _currentPrice = data.c[data.c.length - 1].toFixed(2);
+    const _previousPrice = data.c[data.c.length - 2].toFixed(2);
+    const _priceDiff = (_currentPrice - _previousPrice).toFixed(2);
+    const _priceDiffPercentage = ((_priceDiff / _previousPrice) * 100).toFixed(
+      2
+    );
+    return {
+      _currentPrice,
+      _previousPrice,
+      _priceDiff,
+      _priceDiffPercentage
+    };
+  };
+
+  const prepareChartData = (data) => {
+    const chartData = [];
+    for (let i = 0; i < data.t.length; i++) {
+      chartData.push([data.t[i] * 1000, parseFloat(data.c[i].toFixed(2))]);
+    }
+    return chartData;
+  };
+
+  return (
+    <div className="row mt-3">
+      <div className="col-lg-4 order-2">
+        <ProfileList profile={profile} loading={profileLoading} />
+      </div>
+      <div className="col-lg-8 order-1 mb-3">
+        <StockPrice
+          profile={profile}
+          chartData={chartData}
+          currentPrice={currentPrice}
+          priceDiff={priceDiff}
+          priceDiffPercentage={priceDiffPercentage}
+          loading={priceLoading}
+        />
+      </div>
+    </div>
+  );
 }
